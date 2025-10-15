59f35edecb2eebcf3dbad7e90420dca00b620f80
Namory58 <Namory58@users.noreply.github.com>
2025-10-15 19:08:47 +0000

    deploy: f3cf085903366c45a4856fdaae63849100489197

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 async function chargerCitation() {
   try {
-    const response = await fetch(
-      "https://api.allorigins.win/raw?url=https://zenquotes.io/api/random"
-    );
+    const response = await fetch("https://zenquotes.io/api/random");
     const data = await response.json();
     document.getElementById("citation").innerText = `${data[0].q} — ${data[0].a}`;
   } catch (error) {
