9336989598b22286a2ab35d1d0841456bc0671e2
Namory <namory@Admins-MacBook-Air.local>
2025-10-15 13:34:51 +0200

    affichage de la page

--- src/public/script.js
+++ src/public/script.js
@@ -1,20 +1,16 @@
 async function chargerCitation() {
-  try {
-    const response = await fetch("https://zenquotes.io/api/random");
-
-    if (!response.ok) { // Vérifie que la réponse est correcte
-      throw new Error(`Erreur HTTP : ${response.status}`);
-    }
-
-    const data = await response.json();
-    document.getElementById("citation").innerText = `${data[0].q} — ${data[0].a}`;
-  } catch (error) {
-    document.getElementById("citation").innerText = "Impossible de charger la citation. Réessayez plus tard.";
-    console.error("Erreur fetch :", error);
-  }
+  const response = await fetch("https://zenquotes.io/api/random");
+  const data = await response.json();
+  document.getElementById("citation").innerText = `${data[0].q} — ${data[0].a}`;
 }
+/*
+document.getElementById("nouvelle").addEventListener("click", chargerCitation);
+window.onload = chargerCitation;*/
 
 window.addEventListener("DOMContentLoaded", () => {
   document.getElementById("nouvelle").addEventListener("click", chargerCitation);
   chargerCitation();
 });
+
+
+
