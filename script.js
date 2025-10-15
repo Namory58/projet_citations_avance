6ea1b43ceb78a8215980a0f04771af9d8ffb53cc
Namory58 <Namory58@users.noreply.github.com>
2025-10-15 08:13:00 +0000

    deploy: eae6ccb0fb878cd83fe82c7f3398f249a163af85

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 async function chargerCitation() {
-  const citationElement = document.getElementById("citation");
-  try {
-    const response = await fetch("https://zenquotes.io/api/random");
-    const data = await response.json();
-    citationElement.innerText = `${data[0].q} — ${data[0].a}`;
-  } catch (error) {
-    citationElement.innerText = "Impossible de charger la citation.";
-  }
+  const response = await fetch("https://zenquotes.io/api/random");
+  const data = await response.json();
+  document.getElementById("citation").innerText = `${data[0].q} — ${data[0].a}`;
 }
+
 document.getElementById("nouvelle").addEventListener("click", chargerCitation);
-window.addEventListener("load", chargerCitation);
+window.onload = chargerCitation;
